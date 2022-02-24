2aafe57ee833bfd5ff3bc75493cb691cbbb6f5e9
Rawda Mohamed <rawdam39@gmail.com>
2022-02-24 12:14:14 +0200

    Lab 3 Task 1

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -6,11 +6,7 @@ import { Component, OnInit,Input } from '@angular/core';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
-  @Input() cardItem:any;
-
-
-   availItems : string = '';
-  
+  @Input() cardItem: any;
 
   constructor() { }
 
